cec790c983cc7de557cbc37ecbcd41107644bd65
choudharymahipal <mschoudhary81@gmail.com>
2024-09-26 14:59:49 +0530

    initial commit

---
+++ Admin-LTE-With-DotNet-Core/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Admin_LTE_With_DotNet_Core.Models;
+
+namespace Admin_LTE_With_DotNet_Core.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Admin-LTE-With-DotNet-Core/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Admin_LTE_With_DotNet_Core.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
